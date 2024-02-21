7aac6ae5531ddcde5a9d3885c077d90628decff1
Yusuke Wada <yusuke@kamawada.com>
2024-02-21 10:29:56 +0900

    tweak

--- projects/cloudflare-bindings/vite.config.ts
+++ projects/cloudflare-bindings/vite.config.ts
@@ -2,13 +2,18 @@ import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 import { getPlatformProxy } from 'wrangler'
 
-export default defineConfig(async ({ mode }) => {
-  const proxy = await getPlatformProxy()
+export default defineConfig(async () => {
+  const { env, dispose } = await getPlatformProxy()
   return {
     plugins: [
       honox({
         devServer: {
-          env: proxy.env
+          env,
+          plugins: [
+            {
+              onServerClose: dispose
+            }
+          ]
         }
       })
     ]
